4ecfb5d37a80ef3bbccdeb3392f0cd75e78def4c
iNewbiePlayer <ming.zhou1@ontariotechu.net>
2020-04-13 01:27:19 -0400

    fixed pug files and routed them

--- StockChamp.js
+++ StockChamp.js
@@ -2,19 +2,48 @@ let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+var storedUsers = ["JoeSmith123", "Ken", "admin"]
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
+// change to login page
 app.get('/', (request, response)=>{
-	response.render('header', {});
+	response.render('login', {});
 });
 
+// sign up page
+app.get('/signup', (request, response)=>{
+	let username = request.body.username;
+	console.log(username);
+	response.render('signup', {});
+});
+
+// resolve sign up process
+app.post('/signup', (request, response) =>{
+	//TODO: check if username and email are taken and add to accounts if it hasn't
+});
+
+// change to stock page
 app.get('/stockPage', (request, response)=>{
 	response.render('stockPage', {});
+});
+
+// resolve log in
+app.post('/', (request, response)=>{
+	console.log(request.body.username);
+	console.log(request.body.password);
+	
+	// TODO: check credentials and move to main page if valid
+//	response.render('login', {
+//		verification: false
+//	})
+	
+	response.redirect('/stockPage');
 });
 
 app.set('port', 3000);
