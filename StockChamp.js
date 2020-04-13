9787eb3cd1cac6824f75f81f70755ea07e0a598a
Anushan Vimalathasan <anushan.vimalathasan@uoit.net>
2020-04-13 00:07:48 -0400

    changed stockPage to pug

--- StockChamp.js
+++ StockChamp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -11,6 +11,10 @@ app.set('view engine', 'pug');
 
 app.get('/', (request, response)=>{
 	response.render('header', {});
+});
+
+app.get('/stockPage', (request, response)=>{
+	response.render('stockPage', {});
 });
 
 app.set('port', 3000);
