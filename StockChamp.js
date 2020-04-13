7a8a10e2097564bc816763e460f58b9f04e1499f
Minh Quang Huynh <tony.huynh030699@gmail.com>
2020-04-13 13:44:43 -0400

    Create database and users collection

--- StockChamp.js
+++ StockChamp.js
@@ -1,15 +1,58 @@
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
+let session = require('express-session');
+let uuid = require('uuid/v1');
+let mongoose = require('mongoose');
+let bcrypt = require('bcrypt-nodejs');
+let assert = require('assert');
+let d3   = require('d3');
 
+//Setting up database
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/StockChamp', {
+					useUnifiedTopology: true,
+					useNewUrlParser: true,
+					useCreateIndex: true});
+                 //,{useMongoClient: true});
+
+//Middleware				 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 var storedUsers = ["JoeSmith123", "Ken", "admin"]
 
+//Set view engines
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
+
+//Configure Sessions
+app.use(session({
+	genid: function(request) {
+	  return uuid();
+	},
+	resave: false,
+	saveUninitialized: false,
+	//cookie: {secure: true},
+	secret: "StockChamp"
+}));
+
+// Making database schema
+var Schema = mongoose.Schema;
+
+// Making user table
+var userSchema = new Schema({
+  username:{type: String,
+            unique: true,
+            index: true},
+  hashedPassword: String,
+  isSub: { type: Boolean, default: false},
+  stock1: String,
+  stock2: String,
+  stock3: String
+}, {collection: 'users'});
+var User = mongoose.model('users', userSchema);
 
 // change to login page
 app.get('/', (request, response)=>{
